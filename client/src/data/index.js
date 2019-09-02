c2dbf13480e0af8b68878959c83facdb80b84087
Maksim Ilnitstyi <masimo4work@gmail.com>
2019-09-02 20:31:59 +0300

    Add mock gifts data

--- client/src/data/index.js
+++ client/src/data/index.js
@@ -2,3 +2,158 @@ export const linkMain = { name: 'Main', href: '/' };
 export const linkBasket = { name: 'Basket', href: '/basket' };
 
 export const navMenuLinks = [linkMain, linkBasket];
+
+export const mockGiftsData = [
+  {
+    name: 'Gift 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien,',
+    price: 189,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '1'
+  },
+  {
+    name: 'Gift 2',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna. Cras convallis',
+    price: 35,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '2'
+  },
+  {
+    name: 'Gift 3',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat.',
+    price: 195,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '3'
+  },
+  {
+    name: 'Gift 4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus',
+    price: 33,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '4'
+  },
+  {
+    name: 'Gift 5',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna. Cras convallis convallis dolor. Quisque tincidunt pede ac urna. Ut',
+    price: 152,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '5'
+  },
+  {
+    name: 'Gift 6',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu',
+    price: 191,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '6'
+  },
+  {
+    name: 'Gift 7',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna. Cras convallis convallis dolor. Quisque tincidunt pede ac urna. Ut',
+    price: 127,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '7'
+  },
+  {
+    name: 'Gift 8',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna',
+    price: 64,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '8'
+  },
+  {
+    name: 'Gift 9',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum',
+    price: 16,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '9'
+  },
+  {
+    name: 'Gift 10',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna.',
+    price: 40,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '10'
+  },
+  {
+    name: 'Gift 11',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id,',
+    price: 181,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '11'
+  },
+  {
+    name: 'Gift 12',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna. Cras convallis convallis dolor. Quisque tincidunt pede',
+    price: 33,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '12'
+  },
+  {
+    name: 'Gift 13',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna. Cras convallis convallis dolor. Quisque tincidunt',
+    price: 111,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '13'
+  },
+  {
+    name: 'Gift 14',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna. Cras convallis convallis dolor. Quisque tincidunt pede ac',
+    price: 161,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '14'
+  },
+  {
+    name: 'Gift 15',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna.',
+    price: 40,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '15'
+  },
+  {
+    name: 'Gift 16',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id,',
+    price: 67,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '16'
+  },
+  {
+    name: 'Gift 17',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu',
+    price: 78,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '17'
+  },
+  {
+    name: 'Gift 18',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis',
+    price: 136,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '18'
+  },
+  {
+    name: 'Gift 19',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet',
+    price: 16,
+    img: '@/assets/imgs/gift-img.jpg',
+    id: '19'
+  }
+];
